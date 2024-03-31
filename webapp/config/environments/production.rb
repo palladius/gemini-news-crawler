809faf84ed92f90b4efefff8c073dd3125c21a84
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-03-31 19:16:53 +0200

    0.3 Ive broken my rails app

--- webapp/config/environments/production.rb
+++ webapp/config/environments/production.rb
@@ -50,6 +50,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  #config.force_ssl = false # testing for localhost
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
