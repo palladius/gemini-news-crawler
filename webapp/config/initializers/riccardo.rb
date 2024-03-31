6e09657d92307b491fc3007297f5ab5cae311b57
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-04-01 00:09:41 +0200

    0.8 added footer

--- webapp/config/initializers/riccardo.rb
+++ webapp/config/initializers/riccardo.rb
@@ -1,11 +1,22 @@
 
+APP_NAME = ENV.fetch 'APP_NAME', 'GemiNews'
+APP_VERSION = `cat ./VERSION`.chomp rescue "ERROR: #{$!}"
 
-puts "❌ #{ '❌' * 80}"
 
-puts "❌ Welcome to Riccardo Gemini App. To check that DB is fine, let me paste a few DANGEROUS string:"
-%w{ DATABASE_URL_DEV DATABASE_URL_PROD RAILS_MASTER_KEY}.each do |env_key|
-  puts "❌ ENV[#{env_key}]: #{ ENV.fetch( env_key, '🤷' ).first 5}"
+
+######### Ciao da Riccardo
+emoji = '🧡'
+puts "#{emoji} #{ emoji * 80}"
+
+puts "#{emoji} Welcome to #{APP_NAME} v#{APP_VERSION}"
+puts "#{emoji} To check that DB is fine, let me paste a few DANGEROUS string:"
+# Secret stuff
+%w{ DATABASE_URL_DEV DATABASE_URL_PROD RAILS_MASTER_KEY NEWSAPI_COM_KEY}.each do |env_key|
+  puts "#{emoji} 🕵️‍♂️ ENV[#{env_key}]: #{ ENV.fetch( env_key, '🤷' ).first 5}"
 end
-puts "❌ Which is why I only show the top 5 chars"
-
-puts "❌ #{ '❌' * 80}"
+puts "#{emoji} Which is why I only show the top 5 chars"
+# Public stuff
+%w{ APP_NAME SKAFFOLD_DEFAULT_REPO}.each do |env_key|
+  puts "#{emoji} ☀️ ENV[#{env_key}]: #{ ENV.fetch( env_key, '🤷' )}"
+end
+puts "#{emoji} #{ emoji * 80}"
