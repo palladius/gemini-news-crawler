c88ef6ad0c155970627e2600e27968026f8690b5
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-04-01 11:32:27 +0200

    0.10b better header AGAIN

--- webapp/config/initializers/riccardo.rb
+++ webapp/config/initializers/riccardo.rb
@@ -12,15 +12,15 @@ puts "#{emoji} #{ emoji * 60}"
 puts "#{emoji} Welcome to #{APP_NAME} v#{APP_VERSION}"
 puts "#{emoji} To check that DB is fine, let me paste a few 🕵️‍♂️ SECRET things:"
 # Secret stuff
-%w{ DATABASE_URL_DEV DATABASE_URL_PROD RAILS_MASTER_KEY NEWSAPI_COM_KEY}.each do |env_key|
+%w{ DATABASE_URL_DEV DATABASE_URL_PROD RAILS_MASTER_KEY NEWSAPI_COM_KEY GEMINI_KEY}.sort.each do |env_key|
   puts "#{emoji} 🕵️‍♂️ ENV[#{env_key}]: #{ ENV.fetch( env_key, '🤷' ).first 5}... (size: #{ENV.fetch( env_key, '🤷' ).size})"
 end
-puts "#{emoji} .. which is why I only show the top N chars"
+puts "#{emoji} .. which is why I only show the top N chars. Note that Gemini and NewsAPI keys are useless so far.."
 # Public stuff
 puts "#{emoji}"
 puts "#{emoji} And now the 🌞 PUBLIC stuff:"
 puts "#{emoji} 🌞 Rails.env: #{Rails.env}"
-%w{ APP_NAME SKAFFOLD_DEFAULT_REPO}.each do |env_key|
+%w{ APP_NAME SKAFFOLD_DEFAULT_REPO MESSAGGIO_OCCASIONALE RAILS_ENV}.sort.each do |env_key|
   puts "#{emoji} 🌞 ENV[#{env_key}]: #{ ENV.fetch( env_key, '🤷' )}"
 end
 puts "#{emoji} #{ emoji * 60}"
