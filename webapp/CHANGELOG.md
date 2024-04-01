c926b38f156dac9c85fd3ab24e31a9e2c4f08666
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-04-01 22:32:51 +0200

    0.22 minor tweak in root

--- webapp/CHANGELOG.md
+++ webapp/CHANGELOG.md
@@ -1,2 +1,3 @@
+2024-04-01 v0.22 Now PROD and DEV are 4 whether its magic or not. Not magic = old
 2024-04-01 v0.21 Floating purple 10 articles now looks GREAT
 2024-04-01 v0.20 Update category helper method for link_to in view
--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "articles#index"
+  #root "articles#index"
+  root 'pages#about'
 end
