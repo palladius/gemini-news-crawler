1b7da7b36152d022d3e92e6319d51fe0936c1944
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-05-18 09:16:09 +0200

    0.3.27a nearly works

--- webapp/app/helpers/pages_helper.rb
+++ webapp/app/helpers/pages_helper.rb
@@ -12,4 +12,9 @@ module PagesHelper
     render_prose("# Code result:<br/>\n#{text}", options)
   end
 
+  def render_fancy_step_title(n, title)
+    "<h3 class='text-3xl mb-4 font-bold' >[#️⃣#{n}] #{title}</h3>".html_safe
+  end
+
+
 end
