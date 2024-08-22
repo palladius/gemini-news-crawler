3a7df0a1ff8d6f93bc4c0c9d36f222818db1d047
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-08-22 09:11:50 +0200

    Update rails version to 7.2 and add custom JS file

--- webapp/app/javascript/application.js
+++ webapp/app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// copiato da https://stackoverflow.com/questions/74749172/how-to-use-custom-js-on-a-rails-7-view
+import "custom/main"
