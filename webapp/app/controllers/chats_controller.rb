688443e299c052d63d51fa97d91f91343d1aa043
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-08-23 10:41:01 +0200

    0.3.69 fixing https://github.com/palladius/gemini-news-crawler/issues/2 finally

--- webapp/app/controllers/chats_controller.rb
+++ webapp/app/controllers/chats_controller.rb
@@ -2,10 +2,10 @@
 class ChatsController < ApplicationController
 
 # breaks PROD!
-#   protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 
 
-#     def create
+  def create
 #       CarlessianChat = Data.define(:user, :msg) rescue nil
 #       @turbo_stream = false # Disable Turbo for this action
 #       message = params[:message]
@@ -14,10 +14,10 @@ class ChatsController < ApplicationController
 #       llm_response = "TODO Call your LLM to get the response based on '''#{message}'''"
 #       # TODO move to
 #       render json: { user_message: message, llm_response: llm_response }
-#     end
+  end
 
-#   def index
-#     #@message = '42'
-#   end
+  def index
+    #@message = '42'
+  end
 
 end
